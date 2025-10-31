13344404f3332b6c8e73f46f6479fe6141232723
Damien Pereira <damien.pereira@sysentive.com>
2025-10-31 10:33:14 +0100

    Remove GITHUB_PAGES environment variable dependency and set base path to an empty string in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "/drink-ordering-app/" : "/",
+  base: "",
   plugins: [react()],
   resolve: {
     alias: {
